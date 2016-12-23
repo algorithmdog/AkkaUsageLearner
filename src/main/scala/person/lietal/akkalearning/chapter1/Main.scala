59243c149b25170bc20f455226d1a479057db016
gotoli <gotoli@tencent.com>
2016-12-23 20:31:52 +0800

    ha

--- src/main/scala/person/lietal/akkalearning/chapter1/Main.scala
+++ src/main/scala/person/lietal/akkalearning/chapter1/Main.scala
@@ -3,24 +3,46 @@ package person.lietal.akkalearning.chapter1
 /**
   * Created by lietal on 2016/12/22.
   */
+
+
 import akka.actor.Actor
 import akka.actor.ActorSystem
 import akka.actor.Props
 
-class HelloActor extends Actor {
+object SchoolActorSystem{
+  val actorSystem = ActorSystem("SchoolActorSystem");
+  def getInstance():ActorSystem = {
+    actorSystem
+  }
+}
+
+
+class Student{
+  def keepAsking()={
+    while(true){
+      println("a");
+    }
+  }
+}
+
+class TeacherActor extends Actor {
   def receive = {
-    case "hello" => println("您好！")
-    case _       => println("您是?")
+    case "1+1等于多少?"           => println("2")
+    case "腾讯游戏的格言是什么？" => println("用鑫创造快乐")
+    case "腾讯第一网红是谁？"     => println("哎呀我 watch")
+    case _              => println("这个问题，老师也得查查书")
   }
 }
 
 object Main extends App {
   val system = ActorSystem("HelloSystem")
   // 缺省的Actor构造函数
-  val helloActor = system.actorOf(Props[HelloActor], name = "helloactor")
-  val helloActor1 = system.actorOf(Props[HelloActor], name="helloactor1")
+  val helloActor = system.actorOf(Props[TeacherActor], name = "helloactor")
+  val helloActor1 = system.actorOf(Props[TeacherActor], name="helloactor1")
   helloActor ! "hello"
   helloActor ! "喂"
 
   helloActor1 ! "我操，死了"
+
+
 }
