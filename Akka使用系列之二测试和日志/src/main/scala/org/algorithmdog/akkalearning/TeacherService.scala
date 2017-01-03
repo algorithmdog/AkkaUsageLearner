d14bba3ff50331581bddf88d06fdfc6e8c36ffa1
gotoli <gotoli@tencent.com>
2017-01-03 20:36:51 +0800

    add part2

---
+++ Akka使用系列之二测试和日志/src/main/scala/org/algorithmdog/akkalearning/TeacherService.scala
@@ -0,0 +1,46 @@
+package org.algorithmdog.akkalearning
+
+/**
+  * Created by lietal on 2016/12/22.
+  */
+
+
+import akka.actor.{Actor, ActorSystem, Props}
+import com.typesafe.config.ConfigFactory
+
+class TeacherActor extends Actor {
+  def receive = {
+    case "1+1等于多少?"           => {
+      val originalSender = sender;//
+      sender ! "1+1等于2"
+    }
+    case "历史上规模最大的众筹行动是什么？" => {
+      val originalSender = sender;//
+      sender ! "历史上规模最大的众筹行动是 +1s"
+    }
+    case "腾讯第一网红是谁？"     => {
+      val originalSender = sender;//
+      sender ! "腾讯第一网红是\"我去\""
+    }
+    case _              => {
+      val originalSender = sender;//
+      sender ! "这个问题，老师也得查查书"
+    }
+  }
+}
+
+object TeacherServices extends App {
+
+  val config = ConfigFactory
+    .parseResources("lietal.conf")
+    .getConfig("RemoteServerSideActor")
+  //读入配置
+
+  val system = ActorSystem("TeacherService",  config)
+  //使用配置，建立 Actor 模型系统 ActorSystem。
+  //ActorSystem 是访问 Actor 模型系统的接口，类似于 Spark 的 SparkContext。
+
+  system.actorOf(Props[TeacherActor], "teacherActor")
+  //创建TearcherActor，返回一个引用
+  //这里，我们并不需要使用这个引用
+}
---
+++ Akka使用系列之二测试和日志/src/test/scala/org/algorithmdog/akkalearning/TeacherServiceTest.scala
@@ -0,0 +1,8 @@
+package org.algorithmdog.akkalearning
+
+/**
+  * Created by lietal on 2017/1/3.
+  */
+class TeacherServiceTest {
+
+}
