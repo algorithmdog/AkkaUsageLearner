7c53272b9185dff1ebe786b70f139710a479ca43
lietal <lili1987mail@gmail.com>
2017-02-27 22:42:18 +0800

    add 3

---
+++ Akka使用系列之3层次结构/src/main/scala/org/algorithmdog/akkalearning/hierarchy.scala
@@ -0,0 +1,48 @@
+package org.algorithmdog.akkalearning
+
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
+import com.typesafe.config.ConfigFactory
+
+import scala.concurrent.Await
+import akka.util.Timeout
+
+import scala.concurrent.duration._
+
+/**
+  * Created by lietal on 2016/12/25.
+  */
+
+
+class Master extends Actor with ActorLogging{
+    val english2chinese = context.actorOf(Props[English2Chinese],"English2Chinese")
+    val english2cat     = context.actorOf(Props[English2Cat],"English2Cat")
+
+    def receive = {
+        case eng1:String =>{
+            english2chinese ! eng1
+            english2cat     ! eng1
+             
+        }
+    }
+    
+}
+
+class English2Chinese extends Actor with ActorLogging{
+    def receive = {
+        case eng:String => {
+            println("我翻译不出来!")
+        }
+    }
+}
+
+class English2Cat extends Actor with ActorLogging{
+    def receive = {
+        case eng:String =>{
+             println( "喵喵喵!")
+        }
+    }
+}
+
+
+
+
